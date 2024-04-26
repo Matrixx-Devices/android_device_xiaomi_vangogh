e7cdc1d4007fc1c3cec47274bcac303f521afd1a
Alec Chan <alec3d02@gmail.com>
2024-04-27 01:23:00 +0800

    monet: Reorder device.mk

--- device.mk
+++ device.mk
@@ -4,35 +4,36 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Fingerprint
+TARGET_HAS_UDFPS := true
+
+# Inherit from sm7250-common
+$(call inherit-product, device/xiaomi/sm7250-common/lito.mk)
+
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # Audio configs
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
-
-PRODUCT_SHIPPING_API_LEVEL := 29
 
 # Camera
 PRODUCT_PACKAGES += \
     libMegviiFacepp-0.5.2 \
     libmegface
 
-# Fingerprint
-TARGET_HAS_UDFPS := true
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
-# Inherit from sm7250-common
-$(call inherit-product, device/xiaomi/sm7250-common/lito.mk)
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 29
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/monet/monet-vendor.mk)
