cb5df5e64d81c28f9df111a9be1de65e4a734f68
Alec Chan <alecchangod@gmail.com>
2024-06-09 14:17:43 -0400

    vangogh: import working extract-files.sh from PE *taken from https://github.com/PixelExperience-Devices/device_xiaomi_vangogh/blob/4ee88b2cb81a238dada93f1cea6d854783c85aeb/extract-files.sh *chmod the script to make it executeable

--- extract-files.sh
+++ extract-files.sh
@@ -8,8 +8,45 @@
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib/hw/audio.primary.lito.so)
+            # Before
+            # 07 ad 01 eb  # bl         __android_log_print
+            # 52 b0 01 eb  # bl         send_haptic_data_to_xlog
+            # After
+            # 07 ad 01 eb  # bl         __android_log_print
+            # 00 f0 20 e3  # nop
+            sed -i 's|\x07\xad\x01\xeb\x52\xb0\x01\xeb|\x07\xad\x01\xeb\x00\xf0\x20\xe3|g' "${2}"
+
+            # Before
+            # 53 b0 01 eb  # bl         send_EarsCompensation_dailyuse_to_xlog
+            # 56 b0 01 eb  # bl         send_EarsCompensation_to_xlog
+            # After
+            # 00 f0 20 e3  # nop
+            # 00 f0 20 e3  # nop
+            sed -i 's|\x53\xb0\x01\xeb\x56\xb0\x01\xeb|\x00\xf0\x20\xe3\x00\xf0\x20\xe3|g' "${2}"
+
+            # Before
+            # 04 10 a0 e1  # cpy        r1,r4
+            # 57 b0 01 eb  # bl         send_music_playback_to_xlog
+            # After
+            # 04 10 a0 e1  # cpy        r1,r4
+            # 00 f0 20 e3  # nop
+            sed -i 's|\x04\x10\xa0\xe1\x57\xb0\x01\xeb|\x04\x10\xa0\xe1\x00\xf0\x20\xe3|g' "${2}"
+
+            # Before
+            # 18 10 90 e5  # ldr        r1,[r0,#0x18]
+            # 58 b0 01 eb  # bl         send_misound_data_to_xlog
+            # After
+            # 18 10 90 e5  # ldr        r1,[r0,#0x18]
+            # 00 f0 20 e3  # nop
+            sed -i 's|\x18\x10\x90\xe5\x58\xb0\x01\xeb|\x18\x10\x90\xe5\x00\xf0\x20\xe3|g' "${2}"
+            ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
-            "${SIGSCAN}" -p "9A 0A 00 94" -P "1F 20 03 D5" -f "${2}"
+            hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/210080529A0A0094/210080521F2003D5/g" | xxd -r -p > "${TMPDIR}/${1##*/}"
+            mv "${TMPDIR}/${1##*/}" "${2}"
+            ;;
+        vendor/lib64/camera/components/com.mi.node.watermark.so)
+            patchelf --add-needed "libpiex_shim.so" "${2}"
             ;;
     esac
 }
